462a0455ec11586235294bb74428ec24d9626dd1
Prince Agarwal <princeagarwalofficial@gmail.com>
2026-01-19 15:24:52 +0530

    Added API to return list of five students

--- src/main/java/com/example/demospringboot/controller/home.java
+++ src/main/java/com/example/demospringboot/controller/home.java
@@ -3,13 +3,23 @@ package com.example.demospringboot.controller;
 import com.example.demospringboot.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.ArrayList;
+import java.util.List;
 @RestController
+
 public class home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student = new StudentModel(1,"Prince","prince@gmail.com");
-                return student;
+    public List<StudentModel> getStudents() {
+
+        List<StudentModel> students = new ArrayList<>();
+
+        students.add(new StudentModel(1, "Prince", "prince@gmail.com"));
+        students.add(new StudentModel(2, "Rahul", "rahul@gmail.com"));
+        students.add(new StudentModel(3, "Aman", "aman@gmail.com"));
+        students.add(new StudentModel(4, "Neha", "neha@gmail.com"));
+        students.add(new StudentModel(5, "Simran", "simran@gmail.com"));
+
+        return students;
     }
 
 //    @GetMapping("about/")
